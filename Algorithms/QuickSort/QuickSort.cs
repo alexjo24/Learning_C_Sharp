b0b9e9e76a355755026b323d571a0ffae6d784ff
alexjo24 <alebale2@gmail.com>
2020-12-02 19:03:25 +0100

    QuickSort

---
+++ Algorithms/QuickSort/QuickSort.cs
@@ -0,0 +1,27 @@
+
+public static class QuickSort
+{
+
+    /*
+    How does the algorithm work?
+    It picks some value as a pivot.
+    Reorders the array such that values lower or equal to the pivot are placed before it (lower subarray)
+    Values greater than the pivot are placed after it (higher subarray)
+    This process is called partitioning. Using Hoare partition scheme.
+    The algorithm recursively sorts each of the subarrays.
+    Each subarray is further divided into the next two subarrays and so on.
+    The recursion stops when there are one or zero elements left in a subarray.
+    */
+    static QuickSort(){
+        System.Console.WriteLine("\nQuicksort started .... ");
+
+    }
+
+    public static int[] Run(int[] nonSortedArr){
+
+
+ 
+        return new int[] {1,2};
+    }
+
+}
