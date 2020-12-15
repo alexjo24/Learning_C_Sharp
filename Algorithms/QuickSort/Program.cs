724ca1155566061a8dc26ea5d976c213a6c45cd9
alexjo24 <alebale2@gmail.com>
2020-12-15 13:57:57 +0100

    Fully implemented QuickSort

--- Algorithms/QuickSort/Program.cs
+++ Algorithms/QuickSort/Program.cs
@@ -6,25 +6,34 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Random rnd = new Random();
-            int Min = -10;
-            int Max = 10;
-            int[] randomArr = new int[Max];
+            // Random rnd = new Random();
+            // int Min = -10;
+            // int Max = 10;
+            // int[] randomArr = new int[Max];
 
-            for (int i = 0; i < Max; i++){
-                randomArr[i] = rnd.Next(Min,Max);
-            }
+            // for (int i = 0; i < Max; i++){
+            //     randomArr[i] = rnd.Next(Min,Max);
+            // }
             
+            // System.Console.WriteLine("\nArray to be sorted: ");
+            // foreach (int el in randomArr){
+            //     System.Console.Write(el + " ");
+                
+            // }
+            // System.Console.WriteLine("\nSize of array: " + randomArr.Length);
+
+
+            int[] testArr = new int[] {9, 2 , -4, 5, -1, 1, 6, 2, -4, 9, 2};
+
+
             System.Console.WriteLine("\nArray to be sorted: ");
-            foreach (int el in randomArr){
+            foreach (int el in testArr){
                 System.Console.Write(el + " ");
                 
             }
-            System.Console.WriteLine("\nSize of array: " + randomArr.Length);
-
-
-
-            int[] sortedArr = QuickSort.Run(randomArr);
+            System.Console.WriteLine("\nSize of array: " + testArr.Length);
+            
+            int[] sortedArr = QuickSort.Run(testArr);
             System.Console.WriteLine("\nSorted Array in Ascending order : ");
             foreach (int el in sortedArr){
                 System.Console.Write(el + " ");
--- Algorithms/QuickSort/QuickSort.cs
+++ Algorithms/QuickSort/QuickSort.cs
@@ -17,11 +17,136 @@ public static class QuickSort
 
     }
 
-    public static int[] Run(int[] nonSortedArr){
+    public static int[] Run(int[] arr){
+        int low = 0;
+        int high = arr.Length-1;
 
+        Sort(arr,low,high);
+
+        return arr;
+    }
+
+
+    private static void Sort(int[] arr, int low, int high){
+
+        //Run as long as low is less than high. Otherwise will return out of recursion loop.
+        if (low < high){
+            int pivot = (low+high)/2; //Set pivot id to a element in the middle of the array or close to the middle depending on the size. 
+            int pivot_tmp = Partition(arr,low,high,pivot); //Partition the array. Output where to divide the array into subarrays. 
+
+            
+            //Recursively call on the sort method again to partition subarrays. Pivot_tmp decides where to divide into subarrays.
+            Sort(arr,low,pivot_tmp);//Lower subarray
+            Sort(arr,pivot_tmp+1,high); //Higher subarray
+
+        }else{
+            return;
+        }
+    }
+
+
+    // Partition an given array. Swaps values between elements given an pivot and two pointers.
+    /*
+    *L = "Low" pointer
+    *P = Pivot
+    *H = "High" pointer
+
+    *L starts in the beginning of the array and *H starts in the end of the array. 
+    
+    --> [ 9 , 2 , -4 , 5 , -1 , 1 , 6 , 2 , -4 , 9 , 2 ]
+         *L                     *P                   *H
+
+    *L pointer starts moving first and when *L = value which is bigger than the pivot value. *L pointer stops.
+    *H pointer starts moving until *H = value which is smaller than the pivot value.
+
+    Now we swap values between *L and *H.
+
+    When this is done, *L > High, return *H pointer. 
+    This is used as a limiter for the next subarray in the recursion loop, in the method Sort.
+
+    */
+
+    private static int Partition(int[] arr, int low, int high, int pivot){
 
  
-        return new int[] {1,2};
+        while(low<=high){
+            
+            while (arr[low] < arr[pivot]){
+                low++;
+            }
+
+            while (arr[pivot] < arr[high]){
+                high--;
+            }
+
+            //Break while loop if low index is equal to or bigger than high
+            if (low >= high){
+                break;
+            }
+
+            //Swap values between arr[low] and arr[high].    
+            SwapValues(arr, low, high);
+            low++;
+            high--;
+ 
+            
+        }
+
+        return high;
+        
+        
+        
+        //Code below is one of my tries to get Partition to work.
+        
+        // System.Console.WriteLine("####################################################");
+        // System.Console.WriteLine("####################################################");
+        // System.Console.WriteLine();
+        // System.Console.Write("Arr: " );
+        // foreach (int el in arr){
+        //     System.Console.Write(el + " ");
+        // }
+        // System.Console.WriteLine();
+        // System.Console.WriteLine("low: " + low);
+        // System.Console.WriteLine("high: " + high);
+        // System.Console.WriteLine("pivot value: " + arr[pivot]);
+
+        // bool move_high_pointer = true;
+
+        // while(low < high){
+        //     System.Console.WriteLine();
+        //     System.Console.WriteLine("______________________________________________________");
+        //     System.Console.WriteLine("Pivot value: " + arr[pivot]);
+        //     System.Console.WriteLine("Current low: " + low + ", Current arr[low]: " + arr[low]);
+        //     System.Console.WriteLine("Current high: " + high + ", Current arr[high]: " + arr[high]);
+
+
+        //     if(arr[low] > arr[pivot]){
+        //         move_high_pointer = true;
+                
+        //         while(move_high_pointer){
+
+        //             if(arr[high] < arr[pivot]){
+        //                 SwapValues(arr,low,high);
+        //                 high--;
+        //                 low++;
+        //                 move_high_pointer = false;
+        //             }else{ 
+        //                 high--;
+        //             }
+        //         }
+        //     }else{
+        //         low++;
+        //     }
+        // }
+        // return high;
+    }
+
+    //Method to swap values in an array between two given indices.
+    private static void SwapValues(int[] arr, int low, int high){
+        int tmpVal = arr[low];
+        arr[low] = arr[high];
+        arr[high] = tmpVal;
+
     }
 
 }
